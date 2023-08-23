eb43cd44a4ec1d2a2482785060c55c35575738cc
Sebastian Aristizabal <sebastinn@hotmail.com>
2023-08-23 01:22:43 -0500

    product view list option

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,25 +1,29 @@
 class Product
   # attr_reader :price, :from, :to
-  attr_accessor :name, :price_model, :price, :from, :to
+  attr_accessor :name, :price_model, :price, :tiers
 
   def initialize(product = {})
     @name = product[:name]
     @price_model = product[:price_model]
-    @price = product[:price] || nil if @price_model == "tiered"
+    @price = @price_model == 'fixed' ? product[:price] : nil
     @tiers = product[:tiers] || nil
   end
 
-  def cost
-    if @price_model == "fixed"
-      @price
-    elsif @price_model == "tiered"
-      @tiers.each do |tier|
-        if @from >= tier[:from] && @to <= tier[:to]
-          return tier[:price]
-        end
-      end
-    end
-  end
+  # def cost
+  #   if @price_model == "fixed"
+  #     @price
+  #   elsif @price_model == "tiered"
+  #     @tiers.each do |tier|
+  #       if tier[:from] >= 1 && tier[:to] <= 10
+  #         tier[:price]
+  #       elsif tier[:from] >= 11 && tier[:to] <= 20
+  #         tier[:price]
+  #       elsif tier[:from] >= 21 && tier[:to] <= 30
+  #         tier[:price]
+  #       end
+  #     end
+  #   end
+  # end
 end
 
 # products:
--- app/views/products_view.rb
+++ app/views/products_view.rb
@@ -1,38 +1,22 @@
 class ProductsView
-  # def display(products)
-  #   keys = products.flat_map(&:keys)
-
-  #   p keys
-
-  #   puts "-----------------ALL PRODUCTS---------------------"
-  #   puts "\n"
-
-
-
-  #   keys.uniq.each_with_index do |key, index|
-  #     products.each_with_index do |product|
-
-  #       if key == "orange"
-  #         puts "#{index + 1}. #{key.capitalize} - Price per unit: #{product[key]["price"]} $"
-  #         puts "-------------------------------------------------"
-  #       elsif key == "apple"
-
-  #         puts "#{index + 1}. #{key.capitalize}"
-  #         puts "        Price:"
-  #         product[key]["tiers"].each do |tier|
-  #           puts "              - #{tier["from"]} to #{tier["to"]} units: #{tier["price"]}$"
-  #         end
-  #       end
-  #     end
-  #   end
-  # end
 
   def display(products)
     p products
     puts "-----------------ALL PRODUCTS---------------------"
     puts "\n"
     products.each_with_index do |product, index|
-      puts "#{index + 1} -#{product.name} - #{product.price_model} - #{product.price}$"
+      if product.price_model == "fixed"
+        puts "#{index + 1}. #{product.name.capitalize}"
+        puts "          Price per unit: #{product.price}$"
+
+
+      elsif product.price_model == "tiered"
+        puts "#{index + 1}. #{product.name.capitalize}"
+        puts "        Price:"
+        product.tiers.each do |tier|
+          puts "              -#{tier[:from]} to #{tier[:to]} units: #{tier[:price]}$"
+        end
+      end
     end
   end
 
